1e83c027f88b0e5f296f31271e048a86ce433b32
Samridh Garg <68631593+gsam-7@users.noreply.github.com>
2022-12-31 10:18:54 +0530

    Add files via upload

--- 2bot.js
+++ 2bot.js
@@ -1,45 +1,67 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-let val = 20000;
-// enter the amount of fake votes you want on every run
+let val = process.argv[2];
 
+if (!val) {
+  val = 1;
+}
 
-
-(async () => {
+const threaded = () => {
   for (let i = 0; i < val; i++) {
-    try {
-    const browser = await puppeteer.launch({
-     headless: false,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-    
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);
-    page.on('request', (req) => {
-      if (
-        req.resourceType() == 'sub_frame' ||
-        req.resourceType() == 'stylesheet' ||
-        req.resourceType() == 'font' ||
-        req.resourceType() == 'image'
-      ) {
-        req.abort();
-      } else {
-        req.continue();
+    (async () => {
+      const browser = await puppeteer.launch({ headless: false });
+      try {
+        const page = await browser.newPage();
+        await page.goto('https://ngx.me/4VNu6LV.h?ngxItemID=1980e0a5f5eacce9996483416dbdae7&s=RyJjIjoiZGpqeXU6QkJjdGUubGJtam1hcy56YnRCaWNCcj10RldTdDIxUFpCZGh3c3hxbXZqLnJ5ZSIsInoiOiJ1bXR0cW5sIlQ%3D');
+
+        await setTimeout(async () => {
+          await page.evaluate(() => {
+            $('.xDetailContainer .xButton')[3].click()
+          });
+        }, 500);
+
+        await setTimeout(async () => {
+          await browser.close();
+        }, 500);
+      } catch (e) {
+        console.log(e)
+        await browser.close();
       }
-    });
-    
-    await page.goto(
-      "https://ngx.me/4VNu6LV.h?ngxItemID=1980e0a5f5eacce9996483416dbdae7&s=RyJjIjoiZGpqeXU6QkJjdGUubGJtam1hcy56YnRCaWNCcj10RldTdDIxUFpCZGh3c3hxbXZqLnJ5ZSIsInoiOiJ1bXR0cW5sIlQ%3D", {waitUntil: 'load', timeout: 60000}
-    );
-    await page.evaluate(() => {
-      $(".xDetailContainer .xButton")[3].click();
-    });
-    await setTimeout(async () => {
-      await browser.close();
-      console.log(`Positive ${i}/${val}`)
-    }, 1000);
-    } catch {
-        console.log(`Negative ${i}/${val}`)
-    }
+    })();
   }
-})();
+}
+
+const main = () => {
+  (async () => {
+    for (let i = 0; i < val; i++) {
+      const browser = await puppeteer.launch({ headless: false });
+      try {
+        const page = await browser.newPage();
+        await page.goto('https://ngx.me/4VNu6LV.h?ngxItemID=1980e0a5f5eacce9996483416dbdae7&s=RyJjIjoiZGpqeXU6QkJjdGUubGJtam1hcy56YnRCaWNCcj10RldTdDIxUFpCZGh3c3hxbXZqLnJ5ZSIsInoiOiJ1bXR0cW5sIlQ%3D');
+
+        await setTimeout(async () => {
+          await page.evaluate(() => {
+            $('.xDetailContainer .xButton')[3].click()
+          });
+        }, 500);
+
+        await setTimeout(async () => {
+          await browser.close();
+        }, 500);
+      } catch (e) {
+        console.log(e)
+        await browser.close();
+      }
+    }
+  })();
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('About to crash 😭')
+})
+
+if (process.argv[3]) {
+  main()
+} else {
+  threaded()
+}
